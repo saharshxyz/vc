9fc48ad974f1f5fc5bf6bc7516139327df30bd6a
Saharsh Yeruva <hello@saharsh.tech>
2020-09-13 02:05:08 -0400

    Update Particles

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,41 +21,108 @@ export default function Home() {
 				params={{
 					particles: {
 						number: {
-							value: 60,
+							value: 28,
 							density: {
 								enable: true,
-								value_area: 1500,
+								value_area: 1102.9757735883716,
+							},
+						},
+						color: {
+							value: '#ffffff',
+						},
+						shape: {
+							type: 'circle',
+							stroke: {
+								width: 0,
+								color: '#000000',
+							},
+							polygon: {
+								nb_sides: 5,
+							},
+							image: {
+								src: 'img/github.svg',
+								width: 100,
+								height: 100,
+							},
+						},
+						opacity: {
+							value: 0.5,
+							random: false,
+							anim: {
+								enable: false,
+								speed: 1,
+								opacity_min: 0.1,
+								sync: false,
+							},
+						},
+						size: {
+							value: 3,
+							random: true,
+							anim: {
+								enable: false,
+								speed: 40,
+								size_min: 0.1,
+								sync: false,
 							},
 						},
 						line_linked: {
 							enable: true,
-							opacity: 0.02,
+							distance: 150,
+							color: '#ffffff',
+							opacity: 0.4,
+							width: 1,
 						},
 						move: {
-							direction: 'right',
-							speed: 0.05,
-						},
-						size: {
-							value: 1,
-						},
-						opacity: {
-							anim: {
-								enable: true,
-								speed: 1,
-								opacity_min: 0.05,
+							enable: true,
+							speed: 1,
+							direction: 'none',
+							random: false,
+							straight: false,
+							out_mode: 'out',
+							bounce: false,
+							attract: {
+								enable: false,
+								rotateX: 600,
+								rotateY: 1200,
 							},
 						},
 					},
 					interactivity: {
+						detect_on: 'canvas',
 						events: {
+							onhover: {
+								enable: false,
+								mode: 'repulse',
+							},
 							onclick: {
-								enable: true,
+								enable: false,
 								mode: 'push',
 							},
+							resize: true,
 						},
 						modes: {
+							grab: {
+								distance: 400,
+								line_linked: {
+									opacity: 1,
+								},
+							},
+							bubble: {
+								distance: 400,
+								size: 40,
+								duration: 2,
+								opacity: 8,
+								speed: 3,
+							},
+							repulse: {
+								distance: 200,
+								duration: 0.4,
+							},
 							push: {
-								particles_nb: 1,
+								particles_nb: 4,
+							},
+							remove: {
+								particles_nb: 2,
 							},
 						},
 					},
